d22817ea18f33f40bcecb370a237d93b66ce1f07
BielAlves1 <gabriel2002.alves1010@gmail.com>
2022-11-22 11:25:54 -0300

    ALO

---
+++ docs/forum.sql
@@ -0,0 +1,17 @@
+drop database if exists forum;
+create database forum charset=UTF8 collate utf8_general_ci;
+use forum;
+
+create table usuarios(
+    id_user integer primary auto_increment key not null,
+    email varchar(35) not null unique,
+    senha varchar(20) not null,
+    avatar mediumblob
+);
+
+create table posts(
+    id_user integer auto_increment not null,
+    pergunta varchar(200),
+    reposta varchar(200),
+    foreign key (id_user) references usuarios(id_user)
+);
