6136492811f9f063f6352e90d51dab26a49f85c3
LuccasTxr <luccas.teixeira@gmail.com>
2022-11-24 07:51:09 -0300

    atualizando

--- docs/forum.sql
+++ docs/forum.sql
@@ -6,6 +6,7 @@ create table usuarios(
     id_user integer primary auto_increment key not null,
     email varchar(35) not null unique,
     senha varchar(20) not null,
+    nome_user varchar(30) not null,
     avatar mediumblob
 );
 
@@ -13,5 +14,15 @@ create table posts(
     id_user integer auto_increment not null,
     pergunta varchar(200),
     reposta varchar(200),
+    likes integer,
+    dislikes integer,
     foreign key (id_user) references usuarios(id_user)
 );
+
+create table categorias(
+    id_categoria integer auto_increment not null,
+    tipo varchar(10) not null,
+    nome_categoria varchar(25) not null,
+);
+
+show tables;
