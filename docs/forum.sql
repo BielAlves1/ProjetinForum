10f27788a7af47709d6b598490457e6c71253781
MelroGui <melroguilherme3@gmail.com>
2022-11-25 09:47:26 -0300

    atualizando

--- docs/forum.sql
+++ docs/forum.sql
@@ -7,23 +7,27 @@ create table usuarios(
     email varchar(35) not null unique,
     senha varchar(20) not null,
     nome_user varchar(30) not null,
+    tipo_avatar varchar(5) not null,
     avatar mediumblob
-);
-
-create table posts(
-    id_post integer primary key auto_increment not null,
-    id_user integer not null,
-    pergunta varchar(200),
-    reposta varchar(200),
-    likes integer,
-    dislikes integer,
-    foreign key (id_user) references usuarios(id_user)
 );
 
 create table categorias(
     id_categoria integer primary key auto_increment not null,
     tipo varchar(10) not null,
-    nome_categoria varchar(25) not null
+    nome_categoria varchar(25) not null,
+    qntd_users integer not null
+);
+
+create table posts(
+    id_post integer primary key auto_increment not null,
+    id_user integer not null,
+    id_categoria integer not null,
+    pergunta varchar(200),
+    reposta varchar(200),
+    likes integer,
+    dislikes integer,
+    foreign key (id_user) references usuarios(id_user) on delete cascade,
+    foreign key (id_categoria) references categorias(id_categoria) on delete cascade
 );
 
 describe usuarios;
@@ -32,14 +36,17 @@ describe categorias;
 show tables;
 
 insert into usuarios values
-(default,"bolota@gmail.com","bo1234","bolotagem", default),
-(default,"bebel@gmail.com","bel1234","Bebel cu de mel", default),
-(default,"itimania@gmail.com","it1234","Italo", default);
-insert into posts values
-(default, 4, "biel e guei?","ss!!", 100,1),
-(default, 5, "Vai Brasil","ss!!", 1000,0),
-(default, 6, "Computador Bala","ss!!", 1,10);
+(default,"bolota@gmail.com","bo1234","bolotagem", '.png',load_file("C:/Users/Heitor/Desktop/ProjetinForum/assets/img_561543.png")),
+(default,"bebel@gmail.com","bel1234","Bebel cu de mel", '.png',load_file("C:/Users/Heitor/Desktop/ProjetinForum/assets/img_561543.png")),
+(default,"itimania@gmail.com","it1234","Italo", '.png',load_file("C:/Users/Heitor/Desktop/ProjetinForum/assets/img_561543.png"));
 insert into categorias values
-(default, "Quest", "Duvidas sobre a vida"),
-(default, "Futboll", "VaiBrasil?"),
-(default, "Tecnologia", "TecNews");
+(default, "Quest", "Duvidas sobre a vida", 100),
+(default, "Futboll", "VaiBrasil?", 100),
+(default, "Tecnologia", "TecNews", 100);
+insert into posts values
+(default, 1, 1, "biel e guei?","ss!!", 100,1),
+(default, 2, 2, "Vai Brasil","ss!!", 1000,0),
+(default, 3, 3, "Computador Bala","ss!!", 1,10);
+
+-- select * from 
+-- drop table 
