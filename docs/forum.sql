6c1fb595a6c3ade3ce54335796c190721401342b
BielAlves1 <gabriel2002.alves1010@gmail.com>
2022-12-09 10:35:08 -0300

    aaa

--- docs/forum.sql
+++ docs/forum.sql
@@ -42,10 +42,9 @@ create table posts(
     id_user int not null,
     id_subcat int not null,
     data DATETIME not null,
-    pergunta varchar(1000) not null,    
+    conteudo varchar(1000) not null,    
     likes integer,
     dislikes integer,
-    img mediumblob,
     foreign key (id_user) references usuarios(id_user),
     foreign key (id_subcat) references sub_categorias(id_subcat)
 );
@@ -106,15 +105,40 @@ insert into cargos values
 (default, 'USER');
 
 insert into usuarios values
-(1, 1, 'bebel123@gmail.com', '1234', 'Bielzin'),
-(2, 2, 'guigm@gmail.com', 'senhasenha', 'Bolota'),
-(3, 2, 'juju_bla@gmail.com', '4321', 'JuVamp');
+(default, 1, 'admin', 'admin', 'ADM'),
+(default, 2, 'guigm@gmail.com', 'senhasenha', 'Bolota'),
+(default, 2, 'juju_bla@gmail.com', '4321', 'JuVamp'),
+(default, 2, 'bebel123@gmail.com', '1234', 'Bielzin');
 
 insert into profiles values
 (2, 'Músico, Desenvolvedor e Incel nas horas vagas', 'https://avatars.githubusercontent.com/u/64180881?v=4'),
 (3, 'Web-Designer Desenvolvedora', null );
 
-insert into categorias
-(1, 'Esporte', 'Discutimos sobre qualquer esporte'),
-(2, 'Games', 'Jogos no geral'),
-(3, 'Música', 'Músicas no Geral');
+insert into categorias values
+(default, 'Esporte', 'Discutimos sobre qualquer esporte'),
+(default, 'Games', 'Jogos no geral'),
+(default, 'Música', 'Músicas no Geral');
+
+insert into sub_categorias values
+(default, 1, 'Brasil na Copa', 'Brasil X Coreia'),
+(default, 2, 'GOTY', 'Premiação de Elden Ring'),
+(default, 3, 'Lil Nas X', 'Na direção do League of Legends');
+
+insert into posts values
+(default , 2, 2, '2022/12/08', 'Cleiton Votaria no Bolsonaro?', 0, 999),
+(default , 2, 1, '2022/12/02', 'Odeio molieres na copa', 999, 0),
+(default , 4, 1, '2022/12/02', 'CADÊ O CÁSSIO NA COPA?', 5, 0),
+(default , 3, 3, '2022/12/01', 'O cara viro adm do lolKLKKKKKKKK', 0, 0);
+
+insert into img_posts values
+(1, 'https://pbs.twimg.com/media/EWXF8HPWoAEMuTl?format=png&name=small'),
+(4, 'https://static1-br.millenium.gg/articles/5/11/28/5/@/131432-lil-nas-x-lol-article_image_t-1.png');
+
+insert into comentarios values
+(default, 4, 1, 'Claramente simKLLK', '2022/12/09', 0, 0),
+(default, 3, 2, 'Omi na netKKKL', '2022/12/02', 0, 10),
+(default, 2, 3, 'Na casa da sua mãe', '2022/12/02', 99, 0),
+(default, 4, 4, 'Lil Nas X God apenas.', '2022/12/09', 999, 0);
+
+insert into respostas values
+(default, 2, 1, 'EntãoKKKKKKKK', '')
