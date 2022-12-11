b2db2f95f163e2813cd7e33494213d569434a1d8
MelroGui <melroguilherme3@gmail.com>
2022-12-11 10:37:04 -0300

    atualGM

---
+++ front/scripts/home/home.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector(".criarpost")
+const modal1 = document.querySelector(".menu")
+
+function criarPost() {
+    modal.classList.remove("modal");
+}
+
+function abrirmenu() {
+    modal1.classList.remove("modal1");
+}
